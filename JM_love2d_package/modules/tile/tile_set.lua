1cdef255a722f915d5dcbae01ff0f5da200ea009
Joao Moreira <joaomarcello.mm@gmail.com>
2023-01-07 20:15:46 -0300

    Tileset remove os tiles vazios

--- JM_love2d_package/modules/tile/tile_set.lua
+++ JM_love2d_package/modules/tile/tile_set.lua
@@ -1,52 +1,85 @@
 ---@type JM.Tile
 local Tile = require((...):gsub("tile_set", "tile"))
 
----@class JM.TileSet
-local TileSet = {}
-TileSet.__index = TileSet
+---@param self JM.TileSet
+---@param img_data love.ImageData
+---@param x number
+---@param y number
+local function check_empty(self, img_data, x, y)
+    local acumulator = 0
+    for i = x, x + self.tile_size - 1 do
+        for j = y, y + self.tile_size - 1 do
+            local r, g, b, a = img_data:getPixel(i, j)
+            if a == 0 then
+                acumulator = acumulator + 1
+            else
+                return false
+            end
 
----@param img love.Image
----@param tile_size number|nil
----@return JM.TileSet
-function TileSet:new(img, tile_size)
-    local obj = setmetatable({}, self)
-    TileSet.__constructor__(obj, img, tile_size)
-    return obj
+        end
+    end
+
+    return acumulator >= self.tile_size ^ 2
 end
 
 ---@param self JM.TileSet
-local function load_tiles(self)
+---@param img_data love.ImageData
+local function load_tiles(self, img_data)
+
     local qx = math.floor(self.img_width / self.tile_size)
     local qy = math.floor(self.img_height / self.tile_size)
     local current_id = 1
 
     for j = 1, qy do
         for i = 1, qx do
-            local tile = Tile:new(
-                tostring(current_id),
-                self.img,
-                self.tile_size * (i - 1),
-                self.tile_size * (j - 1),
-                self.tile_size
+            local is_empty = check_empty(self, img_data,
+                (i - 1) * self.tile_size,
+                (j - 1) * self.tile_size
             )
 
-            table.insert(self.tiles, tile)
-            self.id_to_tile[tile.id] = tile
+            if not is_empty then
+                local tile = Tile:new(
+                    tostring(current_id),
+                    self.img,
+                    self.tile_size * (i - 1),
+                    self.tile_size * (j - 1),
+                    self.tile_size
+                )
 
-            current_id = current_id + 1
+                table.insert(self.tiles, tile)
+                self.id_to_tile[tile.id] = tile
+
+                current_id = current_id + 1
+            end
         end
     end
+
+    img_data:release()
 end
 
----@param img love.Image
+---@class JM.TileSet
+local TileSet = {}
+TileSet.__index = TileSet
+
 ---@param tile_size number|nil
-function TileSet:__constructor__(img, tile_size)
-    self.img = img
+---@return JM.TileSet
+function TileSet:new(path, tile_size)
+    local obj = setmetatable({}, self)
+    TileSet.__constructor__(obj, path, tile_size)
+    return obj
+end
+
+---@param path string
+---@param tile_size number|nil
+function TileSet:__constructor__(path, tile_size)
+    local img_data = love.image.newImageData(path)
+    self.img = love.graphics.newImage(img_data)
+
     self.tile_size = tile_size or 32
     self.img_width, self.img_height = self.img:getDimensions()
     self.tiles = {}
     self.id_to_tile = {}
-    load_tiles(self)
+    load_tiles(self, img_data)
 end
 
 ---@param id string
--- test/test_tile.lua
+++ test/test_tile.lua
@@ -1,4 +1,6 @@
 local package = require("/JM_love2d_package/init")
+local Font = package.Font
+
 local Tile = require("/JM_love2d_package/modules/tile/tile")
 local TileSet = require("/JM_love2d_package/modules/tile/tile_set")
 
@@ -8,7 +10,7 @@ local tile_img = love.graphics.newImage("/data/tileset_01.png")
 
 local t1 = Tile:new("1", tile_img, 32 * 1, 32 * 1, 32)
 
-local set = TileSet:new(tile_img)
+local set = TileSet:new("/data/tileset_01.png", 32)
 
 Game:implements({
     update = function(dt)
@@ -17,8 +19,10 @@ Game:implements({
     draw = function()
         t1:draw(32, 64)
 
-        local t = set:get_tile("14")
+        local t = set:get_tile("9")
         local r = t and t:draw(32, 64 * 2)
+
+        Font:print(tostring(#set.tiles), 300, 300)
     end
 })
 
