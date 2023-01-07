3e6f429d48e4d6c063b55ebb22de02dfcbeeff86
Joao Moreira <joaomarcello.mm@gmail.com>
2023-01-07 20:47:43 -0300

    tilemap eh desenhado usando spritebatch

---
+++ JM_love2d_package/modules/tile/tile_map.lua
@@ -0,0 +1,67 @@
+local love_set_color = love.graphics.setColor
+local love_draw = love.graphics.draw
+
+---@type JM.TileSet
+local TileSet = require((...):gsub("tile_map", "tile_set"))
+
+---@class JM.TileMap
+local TileMap = {}
+TileMap.__index = TileMap
+
+---@param path_map string
+---@param path_tileset string
+---@param tile_size number
+function TileMap:new(path_map, path_tileset, tile_size)
+    local obj = setmetatable({}, self)
+    TileMap.__constructor__(obj, path_map, path_tileset, tile_size)
+    return obj
+end
+
+---@alias JM.TileMap.Cell {x:number, y:number, id:string}
+
+---@param path_map string
+---@param path_tileset string
+---@param tile_size number
+function TileMap:__constructor__(path_map, path_tileset, tile_size)
+    self.tile_size = tile_size or 32
+    self.tile_set = TileSet:new(path_tileset, self.tile_size)
+    self.sprite_batch = love.graphics.newSpriteBatch(self.tile_set.img)
+
+    self.map = {}
+    self.map_width = 15
+    self.map_height = 10
+    
+    for j = 1, 10 do
+        self.map[j] = {}
+        for i = 1, 15 do
+            self.map[j][i] = {
+                x = i * self.tile_size,
+                y = j * self.tile_size,
+                id = tostring(math.random(9))
+            }
+        end
+    end
+end
+
+function TileMap:draw()
+    self.sprite_batch:clear()
+
+    for j = 1, 10 do
+        for i = 1, 15 do
+            ---@type JM.TileMap.Cell
+            local cell = self.map[j][i]
+            local tile = self.tile_set:get_tile(cell.id)
+
+            if tile then
+                -- self.sprite_batch:setColor(1, 1, 1, 1)
+                self.sprite_batch:add(tile.quad, cell.x, cell.y)
+            end
+            -- local r = tile and tile:draw(cell.x, cell.y)
+        end
+    end
+
+    love_set_color(1, 1, 1, 1)
+    love_draw(self.sprite_batch)
+end
+
+return TileMap
--- test/test_tile.lua
+++ test/test_tile.lua
@@ -3,6 +3,7 @@ local Font = package.Font
 
 local Tile = require("/JM_love2d_package/modules/tile/tile")
 local TileSet = require("/JM_love2d_package/modules/tile/tile_set")
+local TileMap = require("/JM_love2d_package/modules/tile/tile_map")
 
 local Game = package.Scene:new(nil, nil, nil, nil, 32 * 22, 32 * 12)
 
@@ -12,17 +13,22 @@ local t1 = Tile:new("1", tile_img, 32 * 1, 32 * 1, 32)
 
 local set = TileSet:new("/data/tileset_01.png", 32)
 
+local map = TileMap:new("", "/data/tileset_01.png", 32)
+
 Game:implements({
     update = function(dt)
 
     end,
-    draw = function()
+    draw = function(camera)
         t1:draw(32, 64)
 
         local t = set:get_tile("9")
         local r = t and t:draw(32, 64 * 2)
 
         Font:print(tostring(#set.tiles), 300, 300)
+        Font:print(tostring(camera), 300, 330)
+
+        -- map:draw()
     end
 })
 
